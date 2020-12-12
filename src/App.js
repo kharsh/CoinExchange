3726f23c56839ff11e025caa6d7da262a96ba475
harsh <hkoshti@gmail.com>
2020-12-11 17:16:15 -0800

    CoinExchange

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Coin from './component/Coin/Coin'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={logo} alt="ReactApp"/>
+        <h1 className="App-title">
+          Coin Exchange
+        </h1>
+        </header>
+        <table className="coin-table">
+          <thead>
+            <tr>
+                <th>Name</th>
+                <th>Ticker</th>
+                <th>Price</th>
+            </tr>
+          </thead>
+        <tbody>
+          <Coin name="Bitcoin" ticker="BTC" price={9999.9} />
+          <Coin name="Etherium" ticker="Eth" price={555} />
+          <Coin name="Theater" ticker="USD" price={1} />
+          <Coin name="Riple" ticker="XRP" price={0.2} />
+        </tbody>
+      </table>
     </div>
   );
 }
